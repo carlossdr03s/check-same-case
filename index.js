4f61ee44b29279785a426afc7a59f3912525ccde
carlossdr03s <carlossodreibm@gmail.com>
2023-09-26 11:32:51 -0300

    feat: function to check if its a letter

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+function isLetter(c) {
+  for (let i = 0; i < alphabet.length; i++) {
+    const letter = alphabet[i];
+
+    if (letter === c) {
+      return true
+    }
+
+    return false;
+  }
+}
+
 function sameCase(a, b){
     return 0;
   }
