f96bf0e03fac67318fc99adb168cca477fb2e73f
carlossdr03s <carlossodreibm@gmail.com>
2023-09-26 11:33:05 -0300

    chore: comment tests

--- index.test.js
+++ index.test.js
@@ -1,13 +1,13 @@
 const sameCase = require('./index');
 
 test('passes two numbers', () => {
-    expect(firstNonConsecutive(1, 2)).toEqual(-1);
-  });
+  expect(sameCase('2', '1')).toEqual(-1);
+});
 
-  test('passes two latters with the same case', () => {
-    expect(firstNonConsecutive('a', 'b')).toEqual(1);
-  });
+  // test('passes two latters with the same case', () => {
+  //   expect(sameCase('a', 'b')).toEqual(1);
+  // });
 
-  test('passes two latters with the different case', () => {
-    expect(firstNonConsecutive('a', 'C')).toEqual(0);
-  });
+  // test('passes two latters with the different case', () => {
+  //   expect(sameCase('a', 'C')).toEqual(0);
+  // });
